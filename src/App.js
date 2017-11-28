1aab0dfe2d8d97f4507354eafd8daafb71e60274
Cristiano M. B <cmbergamo@gmail.com>
2017-11-27 22:34:25 -0200

    Criação do primeiro componente;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import Book from './Book'
 
 class BooksApp extends React.Component {
 	state = {
@@ -153,7 +154,7 @@ class BooksApp extends React.Component {
 													</div>
 												</li>
 												<li>
-													<div className="book">
+													{/* <div className="book">
 														<div className="book-top">
 															<div className="book-cover" style={{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' }}></div>
 															<div className="book-shelf-changer">
@@ -168,7 +169,8 @@ class BooksApp extends React.Component {
 														</div>
 														<div className="book-title">Oh, the Places You'll Go!</div>
 														<div className="book-authors">Seuss</div>
-													</div>
+													</div> */}
+													<Book />
 												</li>
 												<li>
 													<div className="book">
