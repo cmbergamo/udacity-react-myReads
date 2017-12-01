f0dfed3363f0a5c319e1a605ba77c6ff33881811
Cristiano M. B <cmbergamo@gmail.com>
2017-12-01 00:26:59 -0200

    Começada a implementação da sinalização de prateleira e alteração da mesma;

--- src/Book.js
+++ src/Book.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
-function Book( props ) {
+class Book extends Component {
 
-	return (
-		<div className="book">
-			<div className="book-top">
-				<Link to='/details'>
-					<div className="book-cover" style={{ width: 128, height: 174, backgroundImage:  `url(${ props.book.imageLinks.smallThumbnail })` }}></div>
-				</Link>
-				<div className="book-shelf-changer">
-					<select>
-						<option value="none" disabled>Move to...</option>
-						<option value="currentlyReading">Currently Reading</option>
-						<option value="wantToRead">Want to Read</option>
-						<option value="read">Read</option>
-						<option value="none">None</option>
-					</select>
+	changeShelf(valeu, id) {
+		console.log(valeu);
+		console.log(id);
+	}
+
+	render () {
+		console.log(this.props.book);
+		const {shelf, id, imageLinks, authors, title} = this.props.book;
+
+		return (
+			<div className="book">
+				{ shelf }
+				<div className="book-top">
+					<Link to='/details'>
+						<div className="book-cover" style={{ width: 128, height: 174, backgroundImage:  `url(${ imageLinks.smallThumbnail })` }}></div>
+					</Link>
+					<div className="book-shelf-changer">
+						<select onChange={ (event) => this.changeShelf(event.target.value , this.props.book.id) } >
+							<option value="none" disabled>Move to...</option>
+							{ shelf === 'currentlyReading' ?
+								(<option value="currentlyReading" selected>Currently Reading</option>) :
+								(<option value="currentlyReading">Currently Reading</option>) 
+							}
+							<option value="wantToRead">Want to Read</option>
+							<option value="read">Read</option>
+							<option value="none">None</option>
+						</select>
+					</div>
 				</div>
+				<div className="book-title"> { this.props.book.title }</div>
+				{ this.props.book.authors && this.props.book.authors.map( (author, index) => 
+					(
+						<div key={ index } className="book-authors">{ author } </div>
+					)
+				)}
 			</div>
-			<div className="book-title"> { props.book.title }</div>
-			{ props.book.authors && props.book.authors.map( (author, index) => 
-				(
-					<div key={ index } className="book-authors">{ author } </div>
-				)
-			)}
-		</div>
-	)
+		)
+	}
 }
 
 export default Book;
